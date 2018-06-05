2cb97bb8f964d419f5e5825887e06c97ca775c54
mhlan <33137338+mhlan@users.noreply.github.com>
2018-06-05 16:50:10 -0700

    Scrapped main.js, refarctored in scripts.js
    Started from the ground up, reorganized everything and re-implimented everything.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,73 @@
 const firstFieldSet = document.querySelector("fieldset");
 const title = document.getElementById("title");
+const design = document.getElementById("design");
+const colorSelections = document
+  .getElementById("color")
+  .querySelectorAll("option");
+let otherTitleCount = 0;
+let y = document.getElementById("design").firstElementChild;
+
 //makes the "name" field the focus on page load/refresh
 function focus() {
   const name = document.getElementById("name").focus();
 }
+
+//hides color selections and replaces with a message to user as default
+function hideColors() {
+  for (let i = 0; i < colorSelections.length; i++) {
+    colorSelections[i].style.display = "none";
+  }
+  if (y.selected === true) {
+    const colorSelection = document.getElementById("color");
+    const option = document.createElement("option");
+    option.setAttribute("id", "theme-select");
+    option.innerText = "Select Theme";
+    colorSelection.append(option);
+    option.selected = "true";
+  }
+}
+
 //create text input field when user selects "Other" from the title role select field
-let otherTitleCount = 0;
 function createOtherTitle() {
   const input = document.createElement("input");
   input.type = "text";
   input.setAttribute("id", "other-title");
   input.setAttribute("placeholder", "Your Title Role");
-  firstFieldSet.append(input);
+  firstFieldSet.appendChild(input);
 }
 
+//listens for changes to the job title selection field
+//dynamically adds or removes text input field for "other" selection
 title.addEventListener("change", e => {
   const titleSelection = e.currentTarget.value;
   if (titleSelection === "other" && otherTitleCount < 1) {
     otherTitleCount++;
     createOtherTitle();
   } else if (titleSelection !== "other" && otherTitleCount > 0) {
-    otherTitleCount--;
     const otherTitle = document.getElementById("other-title");
+    otherTitleCount--;
     otherTitle.remove();
   }
 });
 
+design.addEventListener("change", e => {
+  const designSelection = e.currentTarget.value;
+  if (designSelection === "Select Theme") {
+    hideColors();
+  } else if (designSelection === "js puns") {
+    hideColors();
+    for (let i = 0; i < 3; i++) {
+      let innerText = colorSelections[i];
+      innerText.style.display = "block";
+    }
+  } else if (designSelection === "heart js") {
+    hideColors();
+    for (let i = 3; i < 6; i++) {
+      let innerText = colorSelections[i];
+      innerText.style.display = "block";
+    }
+  }
+});
+
 focus();
+hideColors();
---
+++ js/scripts.js
@@ -0,0 +1,121 @@
+//---- DOM Selectors ----//
+
+const basicInfo = {
+  fieldset: document.querySelector("fieldset"),
+  name: document.getElementById("name"),
+  mail: document.getElementById("mail"),
+  title: document.getElementById("title"),
+  other: document.getElementById("other-title")
+};
+
+const shirtInfo = {
+  fieldset: document.querySelector(".shirt"),
+  size: document.getElementById("size"),
+  design: document.getElementById("design"),
+  colorDiv: document.getElementById("colors-js-puns"),
+  color: document.getElementById("color"),
+  colors: document.getElementById("color").children
+};
+
+const activities = {
+  fieldset: document.querySelector(".activities"),
+  options: document.querySelector(".activities").children,
+  main: document.querySelector(".activities").children[1],
+  frameworks: document.querySelector(".activities").children[2],
+  libraries: document.querySelector(".activities").children[3],
+  express: document.querySelector(".activities").children[4],
+  node: document.querySelector(".activities").children[5],
+  tools: document.querySelector(".activities").children[6],
+  npm: document.querySelector(".activities").children[7]
+};
+
+const payment = {
+  fieldset: document.querySelector(".activities").nextElementSibling,
+  payment: document.getElementById("payment"),
+  cc: document.getElementById("cc-num"),
+  zip: document.getElementById("zip"),
+  cvv: document.getElementById("ccv"),
+  month: document.getElementById("exp-month"),
+  year: document.getElementById("exp-year")
+};
+
+//---- Misc Declarations ----//
+let total = 0;
+
+//---- On Page Load ----//
+
+basicInfo.name.focus();
+basicInfo.other.style.display = "none";
+shirtInfo.design.firstElementChild.disabled = "true";
+shirtInfo.colorDiv.style.display = "none";
+buildTotal();
+
+//---- Functions -----//
+function buildTotal() {
+  if (total === 0) {
+    const span = document.createElement("span");
+    span.id = "total-cost";
+    span.innerText = "Total Due: $" + total;
+    activities.fieldset.append(span);
+  } else if (total > 0) {
+    span.innerText = "Total Due: $" + total;
+  }
+}
+
+function calculateTotal(select) {
+  if (select.name === "all" && select.checked === true) {
+    total += 200;
+  } else if (select.name === "all" && select.checked === false) {
+    total -= 200;
+  } else if (select.type === "checkbox" && select.checked === true) {
+    total += 100;
+  } else if (select.type === "checkbox" && select.checked === false) {
+    total -= 100;
+  }
+  console.log(total);
+}
+
+function updateTotal() {
+  const span = document.getElementById("total-cost");
+}
+
+//---- Event Listeners ----//
+
+//show or hide "other" input form
+basicInfo.title.addEventListener("change", e => {
+  const select = e.target.value;
+  if (select === "other") {
+    basicInfo.other.style.display = "";
+  } else {
+    basicInfo.other.style.display = "none";
+  }
+});
+
+//displays corresponding color options based on selectedd theme
+shirtInfo.design.addEventListener("change", e => {
+  const select = e.target.value;
+  if (select === "js puns") {
+    shirtInfo.colorDiv.style.display = "block";
+    shirtInfo.colors[0].style.display = "";
+    shirtInfo.colors[1].style.display = "";
+    shirtInfo.colors[2].style.display = "";
+    shirtInfo.colors[3].style.display = "none";
+    shirtInfo.colors[4].style.display = "none";
+    shirtInfo.colors[5].style.display = "none";
+    shirtInfo.colors[0].selected = "true";
+  } else if (select === "heart js") {
+    shirtInfo.colorDiv.style.display = "block";
+    shirtInfo.colors[0].style.display = "none";
+    shirtInfo.colors[1].style.display = "none";
+    shirtInfo.colors[2].style.display = "none";
+    shirtInfo.colors[3].style.display = "";
+    shirtInfo.colors[4].style.display = "";
+    shirtInfo.colors[5].style.display = "";
+    shirtInfo.colors[3].selected = "true";
+  }
+});
+
+activities.fieldset.addEventListener("change", e => {
+  const select = e.target;
+  calculateTotal(select);
+});
